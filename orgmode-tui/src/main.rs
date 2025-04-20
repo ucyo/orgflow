9e183efd4af8abc7e858078958fc4e7bf38e91ca
Cayoglu <cayoglu@me.com>
2025-04-20 13:19:37 +0200

    Add first example widget

--- orgmode-tui/src/main.rs
+++ orgmode-tui/src/main.rs
@@ -1,3 +1,51 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use ratatui::{DefaultTerminal, Frame, prelude::Line, style::Stylize, widgets::Widget};
+
+fn main() -> io::Result<()> {
+    // Initialise terminal and move to raw mode
+    let mut terminal = ratatui::init();
+
+    // Create app and run for infinite loop
+    let mut app = App::new();
+    let app_result = app.run(&mut terminal);
+
+    // Disable raw mode
+    ratatui::restore();
+
+    // Return application exit code
+    app_result
+}
+
+struct App {
+    exit: bool,
+}
+
+impl App {
+    fn new() -> Self {
+        App { exit: false }
+    }
+    /// Start the application
+    fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        // Infinite loop until variable set
+        while !self.exit {
+            // Iterate over frames and draw them one by one
+            terminal.draw(|frame| self.draw(frame))?;
+        }
+        Ok(())
+    }
+    /// Routine about how to draw each frame in application
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+}
+
+/// Give App itself the ability to be a Widget (if there is only one widget)
+impl Widget for &App {
+    fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
+    where
+        Self: Sized,
+    {
+        Line::from("Orgmode").bold().render(area, buf);
+    }
 }
